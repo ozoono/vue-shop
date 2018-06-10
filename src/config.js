39989cbeb0d4f5bcbe275a4c248b9312d397aadd
David <ozoono@gmail.com>
2018-06-11 01:23:41 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ export const http = axios.create({
 	baseURL: 'https://gateway.marvel.com:443/v1/public/'
 	//baseURL: 'http://localhost:8080'
 });
-export const apiKey = 'eef4f46e65b1227963d1e2b33d011845';
+export const apiKey = '<YOUR_API_KEY>';
 
 export const availableFormats = ['comic', 'magazine', 'trade paperback', 'graphic novel'];
